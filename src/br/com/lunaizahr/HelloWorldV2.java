9348c410ba42e9b2d3b172ff812551532e17417a
Luna-Izah <LunaIzahR@hotmail.com>
2018-05-03 22:21:19 -0300

    Hello Word

---
+++ src/br/com/lunaizahr/HelloWorldV2.java
@@ -0,0 +1,16 @@
+package br.com.lunaizahr;
+
+import java.util.Random;
+
+
+public class HelloWorldV2 {
+
+    public static void main(String[] args) {
+        int numero = 0; 
+        Random ram = new Random();
+        int meuram = ram.nextInt(); 
+        System.out.println("Hello World");
+        
+    }
+
+}
